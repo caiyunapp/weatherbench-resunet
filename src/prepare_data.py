a0376ef608d99c72aa97ccfe2acf405ae9319248
YiLiu-Lly <64153840+YiLiu-Lly@users.noreply.github.com>
2020-09-01 13:55:41 +0800

    Add files via upload

---
+++ src/prepare_data.py
@@ -0,0 +1,365 @@
+from torch.utils.data import Dataset
+import numpy as np
+import xarray as xr
+import logging
+from config import configs
+import gc
+
+years_train = [
+    # 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989,
+    # 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
+    # 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
+    # 2010, 2011, 2012, 2013, 2014, 2015,
+    2009, 2010, 2011, 2012, 2013, 2014, 2015,
+] # should be in sorted order
+years_eval = [2016]
+years_test =[2017, 2018]
+
+################ prior information
+input_gap = configs.input_gap
+input_length = configs.input_length
+input_span = input_gap * (input_length - 1) + 1
+
+pred_shift = 72  # 72 or 120
+pred_length = configs.output_length 
+pred_gap = pred_shift // pred_length
+#################
+
+logger = logging.getLogger()
+
+class DatasetTrain(Dataset):
+    def __init__(self, years):
+        dio = []
+        dio_solar = []
+        for year in years:
+            logger.info('loading dataset in {0}'.format(year))
+
+            tem_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/temperature_850/tem850_cube_{year}.nc')
+            temp = np.array(tem_data.t)
+            temp = temp[::3]
+
+            t2m_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/T_2m/T_2m_cube_{year}.nc')
+            t2m = np.array(t2m_data.t2m)
+            t2m = t2m[::3]
+
+            z500_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/Z_500/z_500_cube_{year}.nc')
+            z500 = np.array(z500_data.z)
+            z500 = z500[::3]
+ 
+            z1000_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/Z_1000/z_1000_cube_{year}.nc')
+            z1000 = np.array(z1000_data.z)
+            z1000 = z1000[::3]
+
+            thick_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/thickness_300_700/thickness_300_700_cube_{year}.nc')
+            thick = np.array(thick_data.thick)
+            thick = thick[::3]
+
+            if len(dio):                  # if len(dio)!=0
+                dio = np.append(dio, np.stack((temp, t2m, z500, z1000, thick), axis=1), axis=0)
+            else:                         # if len(dio)==0
+                dio = np.stack((temp, t2m, z500, z1000, thick), axis=1)
+            del temp, tem_data, t2m, t2m_data, z500, z500_data, z1000, z1000_data, thick, thick_data
+            if configs.add_now_solar or configs.add_future_solar:           # if configs.add_solar==True
+                solar_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/toa_incident_solar_radiation/solar_radiation_cube_{year}.nc')
+                solar = np.array(solar_data.tisr)
+                solar = solar[::3]
+                if len(dio_solar):
+                    dio_solar = np.append(dio_solar, solar, axis=0)
+                else:
+                    dio_solar = solar
+                del solar, solar_data
+            gc.collect()
+
+        # self.weights_lat = np.cos(np.deg2rad(temp.lat.data))
+        # self.weights_lat /= self.weights_lat.mean()
+
+        #### get index of input and target data
+        input_ind = np.arange(0, input_span, input_gap)          # length : input_length
+        target_ind = np.arange(0, input_span + 2*input_gap, input_gap) + input_span + input_gap - 1   # length : 2*input_length
+        ind = np.concatenate([input_ind, target_ind]).reshape(1, 3*input_length)    # length : 3*input_length
+        max_n_sample = dio.shape[0] - (input_gap*(input_length + 3))    # pred_length = 4*input_gap
+
+        ind = ind + np.arange(max_n_sample)[:, np.newaxis] @ np.ones((1, input_length + 2*input_length), dtype=int)   # pred_length = 2*input_length
+        # @ means Matrix multiplication.   [0 1 2 3 4 5 6 7 8 9 81] + [0:max,1] @ [1,1*11]
+
+        #   input_id & target_id
+        input_id = ind[:,:input_length]
+        target_id = ind[:,input_length:]
+        input_id = input_id.flatten()
+        target_id = target_id.flatten()
+
+        if configs.variable_num==1:
+            _, F, H, W = dio.shape
+        else:
+            _, C, F, H, W = dio.shape
+
+        global dio_mean, dio_std
+        dio_mean = np.mean(dio, axis=(0,2,3,4), keepdims=True)
+        dio_std = np.std(dio, axis=(0,2,3,4), ddof=1, keepdims=True)
+        dio = (dio - dio_mean) / dio_std
+
+        self.di = dio[input_id].reshape(-1, input_length * configs.variable_num, F, H, W)
+        self.do = dio[target_id].reshape(-1, 2*input_length * configs.variable_num, F, H, W)
+
+        # change the order of the channals to [temp1,temp2, t2m1, t2m2, z5001,z5002....]
+        order = []
+        for i in range(configs.variable_num):
+            order.extend([i,i+configs.variable_num])      # order = [0,5,1,6,2,7,3,8,4,9]
+        self.di = self.di[:,order]
+        for i in range(2*configs.variable_num, 3*configs.variable_num):
+            order.extend([i,i+configs.variable_num])      # order = [0,5,1,6,2,7,3,8,4,9,10,15,11,16,12,17,13,18,14,19]
+        self.do = self.do[:,order]
+
+        del dio
+        gc.collect()
+
+        if configs.add_now_solar or configs.add_future_solar:
+            global solar_mean, solar_std
+            solar_mean = np.mean(dio_solar)
+            solar_std = np.std(dio_solar, ddof=1)
+            dio_solar = (dio_solar - solar_mean) / solar_std
+
+            di_solar = dio_solar[input_id].reshape(-1, input_length, F, H, W)
+            do_solar = dio_solar[target_id].reshape(-1, 2*input_length, F, H, W)
+            del dio_solar
+            gc.collect()
+            self.do = np.append(self.do, do_solar, axis=1)
+            del do_solar
+            gc.collect()
+            self.di = np.append(self.di, di_solar, axis=1)
+            del di_solar
+            gc.collect()
+
+        assert (len(self.di.shape) == 5) and (len(self.do.shape) == 5)
+        assert self.di.shape[0] == self.do.shape[0]
+        logger.info('loading finished, input of shape {0}, output of shape {1}'.format(self.di.shape, self.do.shape))
+
+    def __len__(self):
+        return self.di.shape[0]
+
+    def __getitem__(self, index):
+        return self.di[index], self.do[index]
+
+class DatasetEval(Dataset):
+    def __init__(self, years):
+        dio = []
+        dio_solar = []
+        for year in years:
+            logger.info('loading dataset in {0}'.format(year))
+
+            tem_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/temperature_850/tem850_cube_{year}.nc')
+            temp = np.array(tem_data.t)
+            temp = temp[::3]
+
+            t2m_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/T_2m/T_2m_cube_{year}.nc')
+            t2m = np.array(t2m_data.t2m)
+            t2m = t2m[::3]
+ 
+            z500_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/Z_500/z_500_cube_{year}.nc')
+            z500 = np.array(z500_data.z)
+            z500 = z500[::3]
+
+            z1000_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/Z_1000/z_1000_cube_{year}.nc')
+            z1000 = np.array(z1000_data.z)
+            z1000 = z1000[::3]
+
+            thick_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/thickness_300_700/thickness_300_700_cube_{year}.nc')
+            thick = np.array(thick_data.thick)
+            thick = thick[::3]
+
+            if len(dio):                  # if len(dio)!=0
+                dio = np.append(dio, np.stack((temp, t2m, z500, z1000, thick), axis=1), axis=0)
+            else:                         # if len(dio)==0
+                dio = np.stack((temp, t2m, z500, z1000, thick), axis=1)
+            del temp, tem_data, t2m, t2m_data, z500, z500_data, z1000, z1000_data, thick, thick_data
+            if configs.add_now_solar or configs.add_future_solar:           # if configs.add_solar==True
+                solar_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/toa_incident_solar_radiation/solar_radiation_cube_{year}.nc')
+                solar = np.array(solar_data.tisr)
+                solar = solar[::3]
+                if len(dio_solar):
+                    dio_solar = np.append(dio_solar, solar, axis=0)
+                else:
+                    dio_solar = solar
+                del solar, solar_data
+            gc.collect()
+
+        #### get index of input and target data
+        input_ind = np.arange(0, input_span, input_gap)          # length : input_length
+        target_ind = np.arange(0, input_span + 2*input_gap, input_gap) + input_span + input_gap - 1   # length : 2*input_length
+        ind = np.concatenate([input_ind, target_ind]).reshape(1, 3*input_length)    # length : 3*input_length
+        max_n_sample = dio.shape[0] - (input_gap*(input_length + 3))    # pred_length = 4*input_gap
+
+        ind = ind + np.arange(max_n_sample)[:, np.newaxis] @ np.ones((1, input_length + 2*input_length), dtype=int)   # pred_length = 2*input_length
+        # @ means Matrix multiplication.   [0 1 2 3 4 5 6 7 8 9 81] + [0:max,1] @ [1,1*11]
+
+        #   input_id & target_id
+        input_id = ind[:,:input_length]
+        target_id = ind[:,input_length:]
+        input_id = input_id.flatten()
+        target_id = target_id.flatten()
+
+        if configs.variable_num==1:
+            _, F, H, W = dio.shape
+        else:
+            _, C, F, H, W = dio.shape
+
+        dio = (dio - dio_mean) / dio_std
+
+        self.di = dio[input_id].reshape(-1, input_length * configs.variable_num, F, H, W)
+        self.do = dio[target_id].reshape(-1, 2*input_length * configs.variable_num, F, H, W)
+        del dio
+        gc.collect()
+
+        # change the order of the channals to [temp1,temp2, t2m1, t2m2, z5001,z5002....]
+        order = []
+        for i in range(configs.variable_num):
+            order.extend([i,i+configs.variable_num])      # order = [0,5,1,6,2,7,3,8,4,9]
+        self.di = self.di[:,order]
+        for i in range(2*configs.variable_num, 3*configs.variable_num):
+            order.extend([i,i+configs.variable_num])      # order = [0,5,1,6,2,7,3,8,4,9,10,15,11,16,12,17,13,18,14,19]
+        self.do = self.do[:,order]
+
+        if configs.add_now_solar or configs.add_future_solar:
+            dio_solar = (dio_solar - solar_mean) / solar_std
+            di_solar = dio_solar[input_id].reshape(-1, input_length, F, H, W)
+            do_solar = dio_solar[target_id].reshape(-1, 2*input_length, F, H, W)
+            del dio_solar
+            gc.collect()
+            self.do = np.append(self.do, do_solar, axis=1)
+            del do_solar
+            gc.collect()
+            self.di = np.append(self.di, di_solar, axis=1)
+            del di_solar
+            gc.collect()
+
+        assert (len(self.di.shape) == 5) and (len(self.do.shape) == 5)
+        assert self.di.shape[0] == self.do.shape[0]
+        logger.info('loading finished, input of shape {0}, output of shape {1}'.format(self.di.shape, self.do.shape))
+
+    def __len__(self):
+        return self.di.shape[0]
+
+    def __getitem__(self, index):
+        return self.di[index], self.do[index]
+
+class DatasetTest(Dataset):
+    def __init__(self, years):
+        dio = []
+        dio_solar = []
+        target = []
+        for year in years:
+            logger.info('loading dataset in {0}'.format(year))
+
+            tem_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/temperature_850/tem850_cube_{year}.nc')
+            temp = np.array(tem_data.t)
+            temp = temp[::3]
+
+            t2m_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/T_2m/T_2m_cube_{year}.nc')
+            t2m = np.array(t2m_data.t2m)
+            t2m = t2m[::3]
+
+            z500_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/Z_500/z_500_cube_{year}.nc')
+            z500 = np.array(z500_data.z)
+            z500 = z500[::3]
+
+            z1000_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/Z_1000/z_1000_cube_{year}.nc')
+            z1000 = np.array(z1000_data.z)
+            z1000 = z1000[::3]
+
+            thick_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/thickness_300_700/thickness_300_700_cube_{year}.nc')
+            thick = np.array(thick_data.thick)
+            thick = thick[::3]
+
+
+            if len(dio):                  # if len(dio)!=0
+                dio = np.append(dio, np.stack((temp, t2m, z500, z1000, thick), axis=1), axis=0)
+            else:                         # if len(dio)==0
+                dio = np.stack((temp, t2m, z500, z1000, thick), axis=1)
+            if len(target):
+                target = np.append(target, temp, axis=0)
+            else:
+                target = temp
+            del temp, tem_data, t2m, t2m_data, z500, z500_data, z1000, z1000_data, thick, thick_data
+            if configs.add_now_solar or configs.add_future_solar:           # if configs.add_solar==True
+                solar_data = xr.load_dataset(f'/data/weatherbench/CubeSphere/toa_incident_solar_radiation/solar_radiation_cube_{year}.nc')
+                solar = np.array(solar_data.tisr)
+                solar = solar[::3]
+                if len(dio_solar):
+                    dio_solar = np.append(dio_solar, solar, axis=0)
+                else:
+                    dio_solar = solar
+                del solar, solar_data
+            gc.collect()
+
+        #### get index of input and target data
+        input_ind = np.arange(0, input_span, input_gap)          # length : input_length
+        target_ind = np.arange(0, input_span + 10*input_gap, input_gap) + input_span + input_gap - 1   # length : 2*input_length
+        ind = np.concatenate([input_ind, target_ind]).reshape(1, 7*input_length)    # length : 3*input_length
+        max_n_sample = dio.shape[0] - input_gap*(input_length + 11)    # pred_length = 4*input_gap
+
+        ind = ind + np.arange(max_n_sample)[:, np.newaxis] @ np.ones((1, input_length + 6*input_length), dtype=int)   # pred_length = 2*input_length
+        # @ means Matrix multiplication.   [0 1 2 3 4 5 6 7 8 9 81] + [0:max,1] @ [1,1*11]
+
+        input_id = ind[:,:input_length]
+        target_id1 = ind[:,-1:]
+        target_id2 = ind[:,input_length:]
+        input_id = input_id.flatten()
+        target_id1 = target_id1.flatten()
+        target_id2 = target_id2.flatten()
+
+        ####
+        if configs.variable_num==1:
+            _, F, H, W = dio.shape
+        else:
+            _, C, F, H, W = dio.shape
+        
+        # for i in range(configs.variable_num):
+        #     dio[:,i] = norm_z_score(dio[:,i])
+        # print('std of test t850:     ', np.std(target, ddof=1))
+        # print('mean of test t850:     ', np.mean(target))
+
+        # print('mean of variable:     ', dio_mean)
+        # print('std of variable:     ', dio_std)
+
+        dio = (dio - dio_mean) / dio_std
+        target = (target - np.squeeze(dio_mean[0,0])) / np.squeeze(dio_std[0,0])
+        self.di = dio[input_id].reshape(-1, input_length * configs.variable_num, F, H, W)
+        self.do = target[target_id1].reshape(-1, 1, F, H, W)
+
+        # change the order of the channals to [temp1,temp2, t2m1, t2m2, z5001,z5002....]
+        order = []
+        for i in range(configs.variable_num):
+            order.extend([i,i+configs.variable_num])      # order = [0,5,1,6,2,7,3,8,4,9]
+        self.di = self.di[:,order]
+
+        del dio, target
+        gc.collect()
+
+        if configs.add_now_solar or configs.add_future_solar:
+            dio_solar = (dio_solar - solar_mean) / solar_std
+            di_solar = dio_solar[input_id].reshape(-1, input_length, F, H, W)
+            do_solar = dio_solar[target_id2].reshape(-1, 6*input_length, F, H, W)
+            del dio_solar
+            gc.collect()
+            self.do = np.append(self.do, do_solar, axis=1)
+            del do_solar
+            gc.collect()
+            self.di = np.append(self.di, di_solar, axis=1)
+            del di_solar
+            gc.collect()
+
+        assert (len(self.di.shape) == 5) and (len(self.do.shape) == 5)
+        assert self.di.shape[0] == self.do.shape[0]
+        logger.info('loading finished, input of shape {0}, output of shape {1}'.format(self.di.shape, self.do.shape))
+
+    def __len__(self):
+        return self.di.shape[0]
+
+    def __getitem__(self, index):
+        return self.di[index], self.do[index]
+
+# train
+dataset_train = DatasetTrain(years_train)
+# eval and test
+dataset_eval = DatasetEval(years_eval)
+dataset_test = DatasetTest(years_test)
---
+++ src/trainer.py
@@ -0,0 +1,247 @@
+from HyperModel import HyperModel
+# from WarpModel import WarpModel
+# from cubesphere_unet_net import CubeSphereUNet2D
+import os
+import torch
+import torch.nn as nn
+from config import configs
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import logging
+from pathlib import Path
+import numpy as np
+import arrow
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+
+time_str = arrow.now().format('YYYYMMDD_HHmmss')
+model_path = Path(f'./tt-{time_str}')
+model_path.mkdir(exist_ok=True)
+log_file = model_path / Path('train.log')
+logging.basicConfig(level=logging.INFO, filename=log_file, filemode='w', format='%(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.info(configs.__dict__)
+
+from prepare_data import dataset_train, dataset_eval, dataset_test, dio_std
+
+
+continue_training = False     # if continue training, load model
+s=(0,-1,-1)
+
+network = HyperModel()
+if torch.cuda.is_available():
+    network = network.cuda()
+
+network = nn.DataParallel(network, output_device=0)
+
+optimizer = torch.optim.Adam(network.parameters(), lr=configs.lr, weight_decay=configs.weight_decay)
+lr_scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=1, verbose=True, min_lr=0.00004)
+target_ratio = torch.from_numpy(np.squeeze(dio_std[0,0]))
+
+def RMSE(y_pred, y_true):
+    loss = torch.mean((y_pred - y_true) ** 2, dim=[1, 2, 3, 4])
+    loss = torch.mean(torch.sqrt(loss))
+    return loss
+
+def train_once(input_x, input_y):
+    network.train()
+    input_x = input_x.float()
+    input_y = input_y.float()
+    if torch.cuda.is_available():
+        input_x = input_x.cuda()
+        input_y = input_y.cuda()
+
+    optimizer.zero_grad()
+    # add_solar
+    if configs.add_now_solar and configs.add_future_solar:
+        input_x = torch.cat((input_x, input_y[:,-4:-2,:,:,:]), dim=1)     # temperature + now_solar + future_solar
+    elif configs.add_future_solar:                   # if self.configs.add_future_solar and not self.configs.add_now_solar
+        input_x[:,-2:,:,:,:] = input_y[:,-4:-2,:,:,:]                     # temperature + future_solar
+
+    output1 = network(input_x)
+    if configs.add_now_solar and configs.add_future_solar:
+        input2 = torch.cat((output1, input_y[:,-4:,:,:,:]), dim=1)
+        target = input_y[:,0:-4,:,:,:]                    # targets don't include solar
+    elif configs.add_now_solar:
+        input2 = torch.cat((output1, input_y[:,-4:-2,:,:,:]), dim=1) # concatenate temperature & solar
+        target = input_y[:,0:-4,:,:,:]                    # targets don't include solar
+    elif configs.add_future_solar:
+        input2 = torch.cat((output1, input_y[:,-2:,:,:,:]), dim=1)
+        target = input_y[:,0:-4,:,:,:]                    # targets don't include solar
+    else:
+        input2 = output1
+        target = input_y
+    
+    output2 = network(input2)
+    output = torch.cat((output1, output2), dim=1)
+    loss = RMSE(output, target)
+    loss.backward()
+    optimizer.step()
+    return loss.item()
+
+
+def train_all(dataloader_train, dataloader_eval, dataloader_test):
+    count_eval = 0
+    best_eval = 1000
+    count_test = 0
+    best_test = 1000
+    for i in range(configs.num_epochs):
+        logger.info('\nepoch: {0}'.format(i+1))
+        for j, (input_x, input_y) in enumerate(dataloader_train):
+            loss_train = train_once(input_x, input_y)
+
+            if j % configs.display_interval == 0:
+                logger.info('batch training loss: {:.5f}'.format(loss_train))
+
+        # evaluation
+        loss_eval = valid(dataloader_eval)
+        logger.info('epoch eval loss: {:.5f}'.format(loss_eval))
+        lr_scheduler.step(loss_eval)
+        if loss_eval >= best_eval:
+            count_eval += 1
+            logger.info('eval loss is not improved for {0} epoch'.format(count_eval))
+        else:
+            count_eval = 0
+            logger.info('eval loss is improved from {:.5f} to {:.5f}'.format(best_eval, loss_eval))
+            best_eval = loss_eval
+
+        loss_test = test(dataloader_test)
+        logger.info('epoch test loss: {:.5f}'.format(loss_test))
+        if loss_test >= best_test:
+            count_test += 1
+            logger.info('test loss is not improved for {0} epoch'.format(count_test))
+        else:
+            count_test = 0
+            logger.info('test loss is improved from {:.5f} to {:.5f}, saving model'.format(best_test, loss_test))
+            save_model_test()
+            best_test = loss_test
+
+        if count_test == configs.patience:
+            logger.info('early stopping reached, best test loss is {:5f}'.format(best_test))
+            break
+
+
+def valid(dataloader_val):
+    network.eval()
+    loss_val = 0
+    with torch.no_grad():
+        for j, (input_x, input_y) in enumerate(dataloader_val):
+            input_x = input_x.float()
+            input_y = input_y.float()
+            # add_solar
+            if configs.add_now_solar and configs.add_future_solar:
+                input_x = torch.cat((input_x, input_y[:,-4:-2,:,:,:]), dim=1)     # temperature + now_solar + future_solar
+            elif configs.add_future_solar:                   # if self.configs.add_future_solar and not self.configs.add_now_solar
+                input_x[:,-2:,:,:,:] = input_y[:,-4:-2,:,:,:]                     # temperature + future_solar
+            
+            if torch.cuda.is_available():
+                input_x = input_x.cuda()
+                input_y = input_y.cuda()
+
+            output1 = network(input_x)
+
+            if configs.add_now_solar and configs.add_future_solar:
+                input2 = torch.cat((output1, input_y[:,-4:,:,:,:]), dim=1)
+                target = input_y[:,0:-4,:,:,:]                    # targets don't include solar
+            elif configs.add_now_solar:
+                input2 = torch.cat((output1, input_y[:,-4:-2,:,:,:]), dim=1) # concatenate temperature & solar
+                target = input_y[:,0:-4,:,:,:]                    # targets don't include solar
+            elif configs.add_future_solar:
+                input2 = torch.cat((output1, input_y[:,-2:,:,:,:]), dim=1)
+                target = input_y[:,0:-4,:,:,:]                    # targets don't include solar
+            else:
+                input2 = output1
+                target = input_y
+            
+            output2 = network(input2)
+            output = torch.cat((output1, output2), dim=1)
+
+            loss_val += RMSE(output, target) * target.size(0)
+
+    loss_val = loss_val / dataset_eval.__len__()
+    return loss_val.item()
+    
+
+def test(dataloader_test):
+    network.eval()
+    loss_test = 0
+    with torch.no_grad():
+        for j, (test_x, test_y) in enumerate(dataloader_test):
+            test_x = test_x.float()
+            test_y = test_y.float()
+            
+            if torch.cuda.is_available():
+                test_x = test_x.cuda()
+                test_y = test_y.cuda()
+
+            if configs.add_now_solar and configs.add_future_solar:
+                input_x = torch.cat((test_x, test_y[:,-12:-10,:,:,:]), dim=1)
+                target = test_y[:,:1,:,:,:]                                 # target is temperature in 3 days later
+                for i in range(5):
+                    output = network(input_x)
+                    if i<4:
+                        input_x = torch.cat((output, test_y[:,(2*i-12):(2*i-8),:,:,:]), dim=1)
+                    else:
+                        input_x = torch.cat((output, test_y[:,(2*i-12):,:,:,:]), dim=1)
+            elif configs.add_now_solar:
+                input_x = test_x
+                target = test_y[:,:1,:,:,:]                                 # target is temperature in 3 days later
+                for i in range(5):
+                    output = network(input_x)
+                    input_x = torch.cat((output, test_y[:,(2*i-12):(2*i-10),:,:,:]), dim=1)
+            elif configs.add_future_solar:
+                input_x = torch.cat((test_x[:,:-2,:,:,:], test_y[:,-12:-10,:,:,:]), dim=1)
+                target = test_y[:,:1,:,:,:]                                 # target is temperature in 3 days later
+                for i in range(5):
+                    output = network(input_x)
+                    if i<4:
+                        input_x = torch.cat((output, test_y[:,(2*i-10):(2*i-8),:,:,:]), dim=1)
+                    else:
+                        input_x = torch.cat((output, test_y[:,(2*i-10):,:,:,:]), dim=1)
+            else:
+                target = test_y[:,:1,:,:,:]
+                for i in range(5):
+                    output = network(input_x)
+                    input_x = output
+
+            output = network(input_x)
+            loss_test += RMSE(output[:,(configs.output_length-1):configs.output_length,:,:,:], target) * target.size(0)
+
+    loss_test = loss_test / dataset_test.__len__()
+    loss_test = loss_test * target_ratio
+    return loss_test.item()
+    
+def save_model_test():
+    torch.save({'net_test': network.state_dict(), 
+                'optimizer_test':optimizer.state_dict()}, model_path / f'checkpoint.chk')
+
+def load_model_test(chk_path):
+    checkpoint = torch.load(chk_path)
+    network.load_state_dict(checkpoint['net_test'])
+    optimizer.load_state_dict(checkpoint['optimizer_test'])
+
+def continue_train(chk_path):
+    checkpoint = torch.load(chk_path)
+    network.load_state_dict(checkpoint['net_test'])
+    optimizer.load_state_dict(checkpoint['optimizer_test'])
+
+
+if __name__ == '__main__':
+
+    logger.info('loading train dataloader')
+    dataloader_train = DataLoader(dataset_train, batch_size=configs.batch_size, shuffle=True, num_workers=configs.n_cpu)
+    logger.info('loading eval dataloader')
+    dataloader_eval = DataLoader(dataset_eval, batch_size=configs.batch_size, shuffle=False, num_workers=configs.n_cpu)
+    logger.info('loading test dataloader')
+    dataloader_test = DataLoader(dataset_test, batch_size=configs.batch_size, shuffle=False, num_workers=configs.n_cpu)
+
+    if continue_training:
+        continue_train(f'./tt-20200827_113604/checkpoint.chk')
+    
+    train_all(dataloader_train, dataloader_eval, dataloader_test)
+    logger.info('\n######training finished!########\n')
+
+    load_model_test(model_path / f'checkpoint.chk')
+    loss_test = test(dataloader_test)
+    logger.info("test loss from best test model: {0}".format(round(loss_test, 5)))
